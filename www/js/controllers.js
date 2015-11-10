b659d20fa6f283040c62be8b7cf3a13bfe397030
Sony Surahman <sony.surahman98@gmail.com>
2015-11-11 00:39:28 +0700

    update

---
+++ www/js/controllers.js
@@ -0,0 +1,28 @@
+/* global angular, document, window */
+'use strict';
+
+angular.module('starter.controllers', [])
+
+.controller('logCtrl', function($scope, $ionicModal, $ionicPopover, $timeout,ionicMaterialMotion,ionicMaterialInk){
+	$timeout(function() {
+        ionicMaterialMotion.slideUp({
+            selector: '.to-up'
+        });
+    }, 0);
+	ionicMaterialInk.displayEffect();
+})
+.controller('profileCtrl', function($scope, $stateParams, $timeout,ionicMaterialMotion,ionicMaterialInk){
+	// Set Motion
+    $timeout(function() {
+        ionicMaterialMotion.slideUp({
+            selector: '.slide-up'
+        });
+    }, 0);
+
+    $timeout(function() {
+        ionicMaterialMotion.fadeSlideInRight({
+            startVelocity: 3000
+        });
+    }, 700);
+	ionicMaterialInk.displayEffect();
+})
